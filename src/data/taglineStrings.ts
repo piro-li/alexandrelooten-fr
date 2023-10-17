3b4c0006daa75c543d83b786756a103f6249f7f9
Pierre Forcioli <pforcioli@hifield.fr>
2023-10-17 19:46:58 +0200

    Fix typo in sentences

--- src/data/taglineStrings.ts
+++ src/data/taglineStrings.ts
@@ -2,7 +2,7 @@ const taglineStrings: string[] = [
   "À la recherche d'un poste qui me permettra de montrer que j'ai plus d'idées que de cafés renversés !",
   'Mon appareil photo préféré ? Sans aucun doute, mon smartphone ! Je transforme chaque instant en une opportunité.',
   "Ma passion pour la communication égale mon amour pour les voyages. Prêt à m'embarquer dans votre équipe ?",
-  'Je traduis les idées en contenu mémorables',
+  'Je traduis les idées en contenus mémorables',
   'La communication et le tourisme sont mon petit coin de paradis. Je peux être votre touche ensoleillée !',
   "Toujours au fait de l'actualité, je cherche en permanence de nouvelles astuces pour briller.",
   'Totalement accro aux réseaux sociaux, mais pas de cure de désintox en vue !',
